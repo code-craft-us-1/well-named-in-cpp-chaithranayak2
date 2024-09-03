4e2e954f1cda5af9b3cf2308e418c6610cb9792d
Chaithra Nayak <119813807+chaithranayak2@users.noreply.github.com>
2024-09-03 16:17:55 +0000

    Commit5

--- colorpair.cpp
+++ colorpair.cpp
@@ -1,7 +1,8 @@
 #include "colorpair.h"
 #include <cmath>
+#include <string>
 
-using namespace TelCoColorCoder;
+namespace TelCoColorCoder {
 
 const char* MajorColorNames[] = {
     "White", "Red", "Black", "Yellow", "Violet"
@@ -33,7 +34,7 @@ std::string ColorPair::ToString() {
     return colorPairStr;
 }
 
-ColorPair TelCoColorCoder::GetColorFromPairNumber(int pairNumber) {
+ColorPair GetColorFromPairNumber(int pairNumber) {
     int zeroBasedPairNumber = pairNumber - 1;
     MajorColor majorColor = 
         (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
@@ -42,8 +43,14 @@ ColorPair TelCoColorCoder::GetColorFromPairNumber(int pairNumber) {
     return ColorPair(majorColor, minorColor);
 }
 
-int TelCoColorCoder::GetPairNumberFromColor(MajorColor major, MinorColor minor) {
+int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
     return major * numberOfMinorColors + minor + 1;
 }
 
 //add a new feature
+
+}
+
+
+
+
